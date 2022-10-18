b3f414f683df08a8116dca7bc6ee90938ca9187b
AmeyaTawade <ameyatawade0@gmail.com>
2022-10-18 21:44:06 +0530

    Co-authored-by: swastik poojari <swastik21@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-// import { useState, useSyncExternalStore } from "react";
-
+import "./App.css"
 import About from "./components/About";
 import Navbar from "./components/Navbar"; 
 import TextForm from "./components/TextForm"; 
+import React, {useState} from "react";
+import Alert from "./components/Alert ";
 
 
 function App() {
-  // const [Mode, setMode] = useState('light'); // whether dark mode is enabled or not
-  <>
-  return (
-    {/* <Navbar title="TextUtils" aboutText="About TextUtils"/>  */}
-   <Navbar title="TextUtils"/>
-   <div className="container my-3" >
-   <TextForm heading="Enter the text to analyze"/>
-   <About/>
-   </div>
-</>
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
 
-}
+  const [alert, setAlert] = useState(null); 
 
-
+  const showAlert =(message, type)=>{
+    setAlert({
+      msg: message,
+      type :type
+    })
+  }
   
-
+  
+  const toggleMode =()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor='grey'
+      showAlert('Dark Mode has been enabled', 'success')
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white' 
+      showAlert('Dark Mode has been disabled', 'success')
+    }
+  }
+  return (
+    <>
+    {/*  <Navbar title="TextUtils" aboutText="About TextUtils"/>  */ }
+   <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+   <Alert alert={alert} />
+   <div className="container my-3" >
+   <TextForm  heading="Enter the text to analyze" mode={mode} />
+   {/* <About/> */}
+    </div>
+   </> 
+  );
+}
 
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -24,21 +24,23 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
     
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('  ');
     //text = "new text";    wrong way
     //setText("new text");   correct way
   return (
     <>
-    <div className="container"> 
+    <div className="container" style={{color : props.mode==='dark'?'white':'black'}}> 
         <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control my-4" value={text} onChange={handleOnChange} id="myBox" rows="3"></textarea>
+          <textarea className="form-control my-4" value={text} onChange={handleOnChange}  
+          style={{backgroundColor:props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'White':'dark'}} id="myBox" rows="3"
+          placeholder="Enter text here"></textarea>
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
       <button className="btn btn-primary mx-2" onClick={handlelowClick}>Convert to Lowercase</button>
       <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color : props.mode==='dark'?'white':'black'}}>
         <h1>Your text summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} minutes to read</p>
